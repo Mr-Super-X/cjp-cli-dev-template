53be388fcab65aa82e06cab4cf5fe5333d059134
v_jpch <v_jpch.digitalgd.com>
2024-09-24 19:34:38 +0800

    feat: 更新页面复用模板

--- cjp-cli-dev-template-vue3-template-page/template/src/views/home/App.vue
+++ cjp-cli-dev-template-vue3-template-page/template/src/views/home/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="<%= name %>">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-#app {
+.<%= name %> {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
