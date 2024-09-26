d442a16bbf3f5b6dd8c41cd4867c0ff02b2fb477
v_jpch <v_jpch.digitalgd.com>
2024-09-26 16:54:09 +0800

    chore: 更新代码片段模板

---
+++ cjp-cli-dev-template-vue3-section/template/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+      msg: '测试内容'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  color: red;
+}
+</style>
